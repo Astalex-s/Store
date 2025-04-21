084217da01729c682031a2b63913b32f1f30d23e
Алексей <alex-804@yandex.ru>
2025-04-21 21:21:50 +0500

    Project 'store' - Change the index function to the IndexView class 21/04/2025

--- products/views.py
+++ products/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 from products.models import Product, ProductCategory, Basket
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from users.models import User
 
-def index(request):
-    context = {
-        'title': 'Store',
-    }
-    return render(request, 'products/index.html', context)
+from django.views.generic.base import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['title'] = 'Store'
+        return context
 
 def products(request, category_id=None, page_number=1):
     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
